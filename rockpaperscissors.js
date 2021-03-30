1b98f0694d5414a4b957b4818c2c7f6c7cf0333f
Reptilea <spencerpdavis@yahoo.com>
2021-03-30 12:27:47 -0500

    can now play a single game, still needs the best of 5 feature

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,6 +1,17 @@
 //creat a way for the user to input their choice//
-
+//console.log(playerChoice)//
 //create a function that randomly calls either rock paper or scissors//
+
+const computerSelection = computerPlay();
+const playerSelection = prompt("Please choose Rock, Paper, or Scissors");
+const player = (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase());
+const youWin = ("You win! " + player + " beats " + computerSelection + "!");
+const youLose = ("You lose! " + computerSelection + " beats " + player + "!");
+const youTie = ("You Tie! You both played " + player + "!");
+
+
+
+
 function computerPlay () {
  
     const options = ["Rock", "Paper", "Scissors"];
@@ -8,7 +19,7 @@ function computerPlay () {
     return (choice, options[choice]);
 }
 
-console.log(computerPlay())
+//console.log(computerPlay())//
 //create a way for the computer to tell the user what it chose//
 
 //create a way for the computer to determine what wins in each scenario//
@@ -16,3 +27,43 @@ console.log(computerPlay())
 //create a way for the computer to tell the user who won//
 
 //make it repeatable//
+function playRound () {
+
+        
+         if (player === 'Rock' && computerSelection === 'Paper') {
+         return (youLose);
+
+         } else if (player === 'Rock' && computerSelection === 'Scissors') {
+         return (youWin);
+       
+         } else if (player === 'Rock' && computerSelection === 'Rock') {
+         return (youTie);
+         
+         } else if (player === 'Paper' && computerSelection === 'Rock') {
+         return (youWin);
+
+         } else if (player === 'Paper' && computerSelection === 'Paper') {
+         return (youTie);
+
+         } else if (player === 'Paper' && computerSelection === 'Scissors') {
+         return (youLose);
+
+
+         } else if (player === 'Scissors' && computerSelection === 'Rock') {
+         return (youLose);
+
+         } else if (player === 'Scissors' && computerSelection === 'Paper') {
+         return (youWin);
+
+         } else if (player === 'Scissors' && computerSelection === 'Scissors') {
+         return (youTie);
+
+         } else {
+        return("You need to enter Rock, Paper, or Scissors in you want to play")
+
+        }
+}
+       
+       
+       console.log (playRound(playerSelection, computerSelection));
+       
