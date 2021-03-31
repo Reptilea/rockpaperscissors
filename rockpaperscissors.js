03dac6d29f1a7a0823b67d441de9e2be3983f8ef
Reptilea <spencerpdavis@yahoo.com>
2021-03-31 10:17:10 -0500

    Fixed the best of five always playing at least 5 games. This project is complete until a later lesson

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,14 +1,14 @@
-//create input for user choice//
-//assign player choice to variable//
-//create random choice generator for computer//
-//assign computer choice to variable//
-//create function to determine winner or loser//
-//create loop for best of 5//
-//create way of tracking how many wins each side has//
-//assign wins to variable//
-//create statement for overall winner or loser//
-//watch your scopes!!!!!//
-
+//create input for user choice - Done!//
+//assign player choice to variable - Done!//
+//create random choice generator for computer - Done!//
+//assign computer choice to variable - Done!//
+//create function to determine winner or loser - Done!//
+//create loop for best of 5 - Almost there//
+//create way of tracking how many wins each side has - Done!//
+//assign wins to variable - Done!//
+//create statement for overall winner or loser - Done!//
+//watch your scopes!!!!! - I did!!!!!//
+console.log('Ready to play me in a best of 5 rock, paper, scissors match?')
 playerWin = 0;
 computerWin = 0;
   
@@ -21,16 +21,13 @@ computerWin = 0;
 
     function game(playerSelection, computerSelection) {
             
-            while (playerWin + computerWin < 5) {
+            while ((playerWin + computerWin < 5) && (playerWin <= 2) && (computerWin <= 2)) {
                 round(playerSelection, computerSelection)
             }
                 return (complete());
 
-        
-
-
         function round(playerSelection, computerSelection) {
-            playerSelection = prompt('What are you going to choose next?').toLowerCase();
+            playerSelection = prompt('Rock, paper, or scissors?').toLowerCase();
             computerSelection = computerPlay();
             
             if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock')||(playerSelection === 'scissors' && computerSelection === 'paper')) {
@@ -39,24 +36,24 @@ computerWin = 0;
             } else if ((playerSelection === 'rock' && computerSelection === 'paper')|| (playerSelection === 'paper' && computerSelection === 'scissors')||(playerSelection === 'scissors' && computerSelection === 'rock')){
                 computerWin++;
                 console.log('You played ' + playerSelection + ','+ ' but the computer played ' + computerSelection + '. You lost! The score is ' + playerWin + ' to ' + computerWin + '.');
-            } else {
+            } else if (playerSelection === computerSelection) {
                 console.log('Computer played ' + computerSelection + '. You tied! The score is ' + playerWin + ' to ' + computerWin + '.');
-            }
+            } else 
+                console.log("Please in put a valid choice")
+            
         }
-
-        
-
-           
     }
 
     function complete() {
-           if (playerWin > computerWin || playerWin >= 3) {
+           if (playerWin > computerWin) {
            return ('You did it! You won the game! Refresh the browser to play again.')
-       } else if (computerWin > playerWin || computerWin >= 3){
+       } else if (computerWin > playerWin){
            return ('Too bad! You lost the Game! Refresh the browser to play again.')
        } else 
           return ('You tied? How is that even possible?')
     }
+
 let computerSelection = computerPlay(); 
-const playerSelection = prompt('Rock Paper or Scissors?').toLowerCase();
+let playerSelection;
+
     console.log(game(playerSelection, computerSelection));
